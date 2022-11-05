ee06c1280a7405ca859140a2fe5d6ce1ab844cc0
DEAFG0AT <de4fg0at@gmail.com>
2022-11-04 17:38:36 -0700

    Initial commit

---
+++ src/main/java/net/gracemotley/motleysmusicdiscs/MotleysMusicDiscs.java
@@ -0,0 +1,14 @@
+package net.gracemotley.motleysmusicdiscs;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MotleysMusicDiscs implements ModInitializer {
+	public static final String MOD_ID = "motleysmusicdiscs";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/gracemotley/motleysmusicdiscs/MotleysMusicDiscsClient.java
@@ -0,0 +1,10 @@
+package net.gracemotley.motleysmusicdiscs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MotleysMusicDiscsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
