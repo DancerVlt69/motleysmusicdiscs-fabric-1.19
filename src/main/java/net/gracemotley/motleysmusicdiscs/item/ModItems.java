196fe2004c1f11c0d1366e0f0c44803f348b19a8
DEAFG0AT <de4fg0at@gmail.com>
2022-12-17 12:31:36 -0800

    Fixed textures not showing up. Was a typo. Removed test blocks and attempted to add our first disc. I was getting an error about there not being enough arguments in the disc declaration (discovered an additional parameter, lengthInSeconds, in the ModMusicDiscItem.java file and tried to remove it, which it did not like (couldn't build). Next I tried adhering and adding 96 as the track length, which let the game launch, but now the music doesn't play when you put the disc in the jukebox... this could be caused by another issue, as it's happened before in the Forge version, but I honestly can't find the problem. Submitted to Kaupen's help server a few hours ago and nothing back yet.

--- src/main/java/net/gracemotley/motleysmusicdiscs/item/ModItems.java
+++ src/main/java/net/gracemotley/motleysmusicdiscs/item/ModItems.java
@@ -5,7 +5,6 @@ import net.gracemotley.motleysmusicdiscs.MotleysMusicDiscs;
 import net.gracemotley.motleysmusicdiscs.item.custom.ModMusicDiscItem;
 import net.gracemotley.motleysmusicdiscs.sound.ModSounds;
 import net.minecraft.item.Item;
-import net.minecraft.item.MusicDiscItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -15,7 +14,7 @@ public class ModItems {
 
 public static final Item CONTENT_BOBURNHAM_MUSIC_DISC = registerItem("content_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.CONTENT_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE)));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 96));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MotleysMusicDiscs.MOD_ID, name), item);
