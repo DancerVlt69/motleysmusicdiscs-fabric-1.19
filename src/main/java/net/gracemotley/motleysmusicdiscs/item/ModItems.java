4ebe41ad54de9290cd518116c989c0d029722828
DEAFG0AT <de4fg0at@gmail.com>
2022-12-24 09:11:06 -0800

    Updated track lengths to match individual discs.

--- src/main/java/net/gracemotley/motleysmusicdiscs/item/ModItems.java
+++ src/main/java/net/gracemotley/motleysmusicdiscs/item/ModItems.java
@@ -20,55 +20,55 @@ public static final Item COMEDY_BOBURNHAM_MUSIC_DISC = registerItem("comedy_bobu
                 new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
 public static final Item FACETIMEWITHMYMOM_BOBURNHAM_MUSIC_DISC = registerItem("facetimewithmymom_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.FACETIMEWITHMYMOM_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 140));
 public static final Item HOWTHEWORLDWORKS_BOBURNHAM_MUSIC_DISC = registerItem("howtheworldworks_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.HOWTHEWORLDWORKS_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 259));
 public static final Item WHITEWOMANSINSTAGRAM_BOBURNHAM_MUSIC_DISC = registerItem("whitewomansinstagram_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.WHITEWOMANSINSTAGRAM_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 240));
 public static final Item UNPAIDINTERN_BOBURNHAM_MUSIC_DISC = registerItem("unpaidintern_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.UNPAIDINTERN_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 32));
 public static final Item BEZOSI_BOBURNHAM_MUSIC_DISC = registerItem("bezosi_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.BEZOSI_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 58));
 public static final Item SEXTING_BOBURNHAM_MUSIC_DISC = registerItem("sexting_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.SEXTING_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 200));
 public static final Item LOOKWHOSINSIDEAGAIN_BOBURNHAM_MUSIC_DISC = registerItem("lookwhosinsideagain_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.LOOKWHOSINSIDEAGAIN_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 77));
 public static final Item PROBLEMATIC_BOBURNHAM_MUSIC_DISC = registerItem("problematic_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.PROBLEMATIC_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 193));
 public static final Item THIRTY_BOBURNHAM_MUSIC_DISC = registerItem("thirty_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.THIRTY_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 154));
 public static final Item DONTWANNAKNOW_BOBURNHAM_MUSIC_DISC = registerItem("dontwannaknow_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.DONTWANNAKNOW_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 63));
 public static final Item SHIT_BOBURNHAM_MUSIC_DISC = registerItem("shit_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.SHIT_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 77));
 public static final Item WELCOMETOTHEINTERNET_BOBURNHAM_MUSIC_DISC = registerItem("welcometotheinternet_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.WELCOMETOTHEINTERNET_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 275));
 public static final Item BEZOSII_BOBURNHAM_MUSIC_DISC = registerItem("bezosii_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.BEZOSII_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 45));
 public static final Item THATFUNNYFEELING_BOBURNHAM_MUSIC_DISC = registerItem("thatfunnyfeeling_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.THATFUNNYFEELING_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 304));
 public static final Item ALLEYESONME_BOBURNHAM_MUSIC_DISC = registerItem("alleyesonme_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.ALLEYESONME_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 217));
 public static final Item GOODBYE_BOBURNHAM_MUSIC_DISC = registerItem("goodbye_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.GOODBYE_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 252));
 public static final Item ANYDAYNOW_BOBURNHAM_MUSIC_DISC = registerItem("anydaynow_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.ANYDAYNOW_BOBURNHAM,
-                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 56));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MotleysMusicDiscs.MOD_ID, name), item);
