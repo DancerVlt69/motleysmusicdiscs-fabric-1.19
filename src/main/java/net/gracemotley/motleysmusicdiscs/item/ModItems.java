9812cbfbfe36d97842c56088e8c5c9c14abf9195
DEAFG0AT <de4fg0at@gmail.com>
2022-12-24 09:00:04 -0800

    Fixed sound issue, was a typo on every disc in sounds.json. Added all Inside discs, Lovejoy is next.

--- src/main/java/net/gracemotley/motleysmusicdiscs/item/ModItems.java
+++ src/main/java/net/gracemotley/motleysmusicdiscs/item/ModItems.java
@@ -18,6 +18,57 @@ public static final Item CONTENT_BOBURNHAM_MUSIC_DISC = registerItem("content_bo
 public static final Item COMEDY_BOBURNHAM_MUSIC_DISC = registerItem("comedy_boburnham_music_disc",
         new ModMusicDiscItem(7, ModSounds.COMEDY_BOBURNHAM,
                 new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item FACETIMEWITHMYMOM_BOBURNHAM_MUSIC_DISC = registerItem("facetimewithmymom_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.FACETIMEWITHMYMOM_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item HOWTHEWORLDWORKS_BOBURNHAM_MUSIC_DISC = registerItem("howtheworldworks_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.HOWTHEWORLDWORKS_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item WHITEWOMANSINSTAGRAM_BOBURNHAM_MUSIC_DISC = registerItem("whitewomansinstagram_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.WHITEWOMANSINSTAGRAM_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item UNPAIDINTERN_BOBURNHAM_MUSIC_DISC = registerItem("unpaidintern_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.UNPAIDINTERN_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item BEZOSI_BOBURNHAM_MUSIC_DISC = registerItem("bezosi_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.BEZOSI_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item SEXTING_BOBURNHAM_MUSIC_DISC = registerItem("sexting_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.SEXTING_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item LOOKWHOSINSIDEAGAIN_BOBURNHAM_MUSIC_DISC = registerItem("lookwhosinsideagain_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.LOOKWHOSINSIDEAGAIN_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item PROBLEMATIC_BOBURNHAM_MUSIC_DISC = registerItem("problematic_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.PROBLEMATIC_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item THIRTY_BOBURNHAM_MUSIC_DISC = registerItem("thirty_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.THIRTY_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item DONTWANNAKNOW_BOBURNHAM_MUSIC_DISC = registerItem("dontwannaknow_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.DONTWANNAKNOW_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item SHIT_BOBURNHAM_MUSIC_DISC = registerItem("shit_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.SHIT_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item WELCOMETOTHEINTERNET_BOBURNHAM_MUSIC_DISC = registerItem("welcometotheinternet_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.WELCOMETOTHEINTERNET_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item BEZOSII_BOBURNHAM_MUSIC_DISC = registerItem("bezosii_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.BEZOSII_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item THATFUNNYFEELING_BOBURNHAM_MUSIC_DISC = registerItem("thatfunnyfeeling_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.THATFUNNYFEELING_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item ALLEYESONME_BOBURNHAM_MUSIC_DISC = registerItem("alleyesonme_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.ALLEYESONME_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item GOODBYE_BOBURNHAM_MUSIC_DISC = registerItem("goodbye_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.GOODBYE_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
+public static final Item ANYDAYNOW_BOBURNHAM_MUSIC_DISC = registerItem("anydaynow_boburnham_music_disc",
+        new ModMusicDiscItem(7, ModSounds.ANYDAYNOW_BOBURNHAM,
+                new FabricItemSettings().group(ModItemGroup.DISCS).maxCount(1).rarity(Rarity.RARE), 314));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MotleysMusicDiscs.MOD_ID, name), item);
--- src/main/java/net/gracemotley/motleysmusicdiscs/sound/ModSounds.java
+++ src/main/java/net/gracemotley/motleysmusicdiscs/sound/ModSounds.java
@@ -8,6 +8,23 @@ import net.minecraft.util.registry.Registry;
 public class ModSounds {
     public static SoundEvent CONTENT_BOBURNHAM = registerSoundEvent("content_boburnham");
     public static SoundEvent COMEDY_BOBURNHAM = registerSoundEvent("comedy_boburnham");
+    public static SoundEvent FACETIMEWITHMYMOM_BOBURNHAM = registerSoundEvent("facetimewithmymom_boburnham");
+    public static SoundEvent HOWTHEWORLDWORKS_BOBURNHAM = registerSoundEvent("howtheworldworks_boburnham");
+    public static SoundEvent WHITEWOMANSINSTAGRAM_BOBURNHAM = registerSoundEvent("whitewomansinstagram_boburnham");
+    public static SoundEvent UNPAIDINTERN_BOBURNHAM = registerSoundEvent("unpaidintern_boburnham");
+    public static SoundEvent BEZOSI_BOBURNHAM = registerSoundEvent("bezosi_boburnham");
+    public static SoundEvent SEXTING_BOBURNHAM = registerSoundEvent("sexting_boburnham");
+    public static SoundEvent LOOKWHOSINSIDEAGAIN_BOBURNHAM = registerSoundEvent("lookwhosinsideagain_boburnham");
+    public static SoundEvent PROBLEMATIC_BOBURNHAM = registerSoundEvent("problematic_boburnham");
+    public static SoundEvent THIRTY_BOBURNHAM = registerSoundEvent("thirty_boburnham");
+    public static SoundEvent DONTWANNAKNOW_BOBURNHAM = registerSoundEvent("dontwannaknow_boburnham");
+    public static SoundEvent SHIT_BOBURNHAM = registerSoundEvent("shit_boburnham");
+    public static SoundEvent WELCOMETOTHEINTERNET_BOBURNHAM = registerSoundEvent("welcometotheinternet_boburnham");
+    public static SoundEvent BEZOSII_BOBURNHAM = registerSoundEvent("bezosii_boburnham");
+    public static SoundEvent THATFUNNYFEELING_BOBURNHAM = registerSoundEvent("thatfunnyfeeling_boburnham");
+    public static SoundEvent ALLEYESONME_BOBURNHAM = registerSoundEvent("alleyesonme_boburnham");
+    public static SoundEvent GOODBYE_BOBURNHAM = registerSoundEvent("goodbye_boburnham");
+    public static SoundEvent ANYDAYNOW_BOBURNHAM = registerSoundEvent("anydaynow_boburnham");
 
 
 
